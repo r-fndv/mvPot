2d806244f5a2c9ddeead09a92306316309b01c7a
Raphaël <rdefondeville@gmail.com>
2017-08-28 15:59:40 +0200

    Update for version 0.1.3

--- src/mvnCppAdaptedSTL.cpp
+++ src/mvnCppAdaptedSTL.cpp
@@ -113,6 +113,7 @@ void pointEstimate(int j,
     Free(y);
 }
 
+
 extern "C" void mvtNormCpp(int *tmp_xn, int *tmp_d, double *tmp_mat, double *tmp_b, double *tmp_generatingVector, double *est, double *err) {
 
     //double diff = 0, p = 0, error = 0;
@@ -145,7 +146,7 @@ extern "C" void mvtNormCpp(int *tmp_xn, int *tmp_d, double *tmp_mat, double *tmp
     //PARAMETERS FOR VARIABLE REORDERING
     std::vector< double > y(*tmp_d);
     std::vector< std::vector<double> > L(*tmp_d, std::vector<double>(*tmp_d));
-    int pos = -1;
+    int pos = 0;
     double min = LONG_MAX;
 
     //PARAMETERS FOR INTEGRATION
@@ -273,13 +274,243 @@ extern "C" void mvtNormCpp(int *tmp_xn, int *tmp_d, double *tmp_mat, double *tmp
     *err = error;
 }
 
+void pointEstimateTProb(int j,
+                        int *d,
+                        const double *generatingVector,
+                        const double *randomShift,
+                        const double *b_org,
+                        const double *L,
+                        double *est,
+                        double *nu)
+{   double value = 0;
+    double* x;
+    x = (double*) Calloc(*d, double);
+    
+    for (int k = 0; k < *d; k++) {
+        x[k] = std::abs(2 * ((generatingVector[k] * j +  randomShift[k])  - floor(generatingVector[k] * j +  randomShift[k])) - 1);
+        
+    }
+    
+    double* e;
+    e = (double*) Calloc(*d, double);
+    double* y;
+    y = (double*) Calloc(*d, double);
+    double* b;
+    b = (double*) Calloc(*d, double);
+    
+    double scaling = 0;
+    scaling = sqrt(2 * qgamma(x[*d - 1], *nu / 2, 1, 1, 0));
+    
+    b[0] = scaling * b_org[0];
+    //b[0] = b_org[0];
+    
+    //e[0] = 0.5 + 0.5 * erf_R((b)[0] / ((L)[0]*sqrt(2.0)));
+    e[0] = pnorm(((b)[0] / (L)[0]), 0, 1, 1, 0);
+    value = e[0] ;
+    
+    for (int k = 1; k < *d; k++) {
+        
+        b[k] = scaling * b_org[k];
+        //b[k] = b_org[k];
+        
+        y[k-1] = stdnormal_inv(e[k - 1] * x[k - 1]);
+        
+        //DEAL WITH INFINTE BOUNDS
+        if(!(R_FINITE(y[k-1]))) {
+            if(y[k-1] > 0){
+                value = 1;
+            } else {
+                value = 0;
+            }
+            break;
+        }
+        
+        double tmp = 0;
+        for (int l = 0; l < k; l++){
+            tmp += (L)[k * (*d) + l] * y[l];
+        }
+        
+        //e[k] = 0.5 + 0.5 * erf_R(((b)[k] - tmp) / ((L)[k * (*d) + k] * sqrt(2.0)));
+        e[k] = pnorm(((b)[k] - tmp) / ((L)[k * (*d) + k]), 0, 1, 1, 0);
+        value = value * e[k];
+        
+    }
+    *est += value;
+    
+    Free(x);
+    Free(e);
+    Free(y);
+}
+
+
+
+extern "C" void mvTProbCpp(int *tmp_xn, int *tmp_d, double *tmp_mat, double *tmp_b, double *nu, double *tmp_generatingVector, double *est, double *err) {
+
+    //double diff = 0, p = 0, error = 0;
+    //double pi = 3.141592653589793;
+
+
+
+    std::vector< double > b(*tmp_d);
+    std::vector< double > generatingVector(*tmp_d);
+    std::vector< std::vector<double> > mat(*tmp_d, std::vector<double>(*tmp_d));
+    for(int i = 0; i < *tmp_d; i++){
+        b[i] = tmp_b[i] / sqrt(*nu);
+        generatingVector[i] = tmp_generatingVector[i];
+        for(int j = 0; j < *tmp_d; j++){
+            mat[i][j] = tmp_mat[i * *tmp_d + j];
+        }
+    }
+
+
+    //DATA PREPARATION
+    for(int i = 0; i < *tmp_d; i++){
+        double t = std::sqrt(mat[i][i]);
+        b[i] = b[i] / t;
+        for( int j = 0; j < *tmp_d; j++){
+            mat[i][j] = mat[i][j] / t;
+            mat[j][i] = mat[j][i] / t;
+        }
+    }
+
+    //PARAMETERS FOR VARIABLE REORDERING
+    std::vector< double > y(*tmp_d);
+    std::vector< std::vector<double> > L(*tmp_d, std::vector<double>(*tmp_d));
+    int pos = 0;
+    double min = LONG_MAX;
+
+    //PARAMETERS FOR INTEGRATION
+    int nRep = 10;
+    double diff = 0, p = 0, error = 0;
+
+
+    //--------------------------------------------------- VARIABLE REORDERING --------------------------------------------
+    //FIND THE FIRST POSITION
+    for (int itPos = 0; itPos < *tmp_d; ++itPos){
+        double b_phi = exp(-pow(b[itPos],2)/2)/ sqrt(2 * M_PI);
+        //double b_Phi = 0.5 + 0.5 * erf_R(b[itPos] / sqrt(2.0));
+        double b_Phi = pnorm(b[itPos], 0, 1, 1, 0);
+        double v = 1 - b[itPos] * b_phi / b_Phi - pow(b_phi/b_Phi,2);
+        if(v < min) {
+            min = v;
+            pos = itPos;
+            y[0] = - b_phi / b_Phi;
+        }
+    }
+
+    //SWITCH POSITIONS 0 and in upper bound
+    std::swap(b[0], b[pos]);
+
+    //SWITCH ROWS AND COLUMNS
+    std::swap(mat[0], mat[pos]);
+    for(int i = 0; i < *tmp_d; ++i){
+        std::swap(mat[i][0], mat[i][pos]);
+    }
+
+    //COMPUTE CHOLESKY
+    L[0][0] = std::sqrt(mat[0][0]);
+    for(int i = 1 ; i < *tmp_d ; i++) L[i][0] = mat[i][0]/L[0][0];
+
+
+
+    //ITERATE THE PROCESS
+    for (int rec = 1; rec < *tmp_d; rec ++){
+        min = LONG_MAX;
+        pos = rec;
+        //------------------COMPUTE CANDDIDATE FOR NEW UPPERBOUND
+        std::vector< double > b_New_Temp(*tmp_d - rec);
+        int count = 0;
+        for (int itPos = rec; itPos < *tmp_d; ++itPos){
+            double conditionnalSum = 0;
+            double squaredSum = 0;
+            for(int i = 0; i < rec; i++) {
+                conditionnalSum += L[itPos][i] * y[i];
+                squaredSum += L[itPos][i] * L[itPos][i];
+            }
+            b_New_Temp[count] = (b[itPos] - conditionnalSum) / sqrt(mat[itPos][itPos] - squaredSum);
+            count++;
+        }
+
+        //FIND THE MINUMUM
+        for (int itPos = 0; itPos < *tmp_d - rec; ++itPos){
+            double b_phi = exp(-pow(b_New_Temp[itPos],2)/2)/ sqrt(2 * M_PI);
+            //double b_Phi = 0.5 + 0.5 * erf_R(b_New_Temp[itPos] / sqrt(2.0));
+            double b_Phi = pnorm(b_New_Temp[itPos], 0, 1, 1, 0);
+            double v = 1 - b_New_Temp[itPos] * b_phi / b_Phi - pow(b_phi/b_Phi,2);
+            if(v < min) {
+                min = v;
+                pos = itPos + rec;
+                y[rec] = - b_phi / b_Phi;
+            }
+        }
+
+        //------------------SWITCH POSITIONS
+        //SWITCH POSITIONS 0 and in upper bound
+        std::swap(b[rec], b[pos]);
+
+        //SWITCH ROWS AND COLUMNS
+        std::swap(mat[rec], mat[pos]);
+        for(int i = 0; i < *tmp_d; ++i){
+            std::swap(mat[i][rec], mat[i][pos]);
+        }
+        std::swap(L[rec], L[pos]);
+
+        //----------------COMPUTE CHOLESKY TERMS
+        double rowSum = 0;
+        for(int j = 0; j < rec; j++) rowSum += L[rec][j] * L[rec][j];
+        L[rec][rec] = std::sqrt(mat[rec][rec] - rowSum);
+        for(int i = rec + 1 ; i < *tmp_d ; i++) {
+            double lProd = 0;
+            for(int k = 0; k < rec; k++) lProd += L[rec][k] * L[i][k];
+            L[i][rec] = (mat[i][rec] - lProd) / L[rec][rec];
+        }
+    }
+
+    //--------------------------------------------------- INTEGRATION ROUTINE --------------------------------------------
+    std::vector< double > vecL;
+    for (int i = 0;i < *tmp_d; i++) {
+        vecL.insert(vecL.end(), L[i].begin(), L[i].end());
+    }
+
+    int *h_d = tmp_d;
+    double *h_generatingVector = tmp_generatingVector, *h_randomShift = (double*) R_alloc(*tmp_d, sizeof(double)), *h_b = &b[0], *h_L =&vecL[0], *h_est = est;
+
+    GetRNGstate();
+
+    for (int i = 0; i < nRep; i++) {
+
+        for (int k = 0; k < *tmp_d; k++){
+            //h_randomShift[k] = ((double) rand() / (RAND_MAX));
+            h_randomShift[k] = unif_rand();
+        }
+        *h_est = 0;
+
+        for (int j = 0; j < *tmp_xn; j++) {
+            pointEstimateTProb(j, h_d, h_generatingVector, h_randomShift, h_b, h_L, h_est, nu);
+        }
+        diff = ((*h_est / *tmp_xn) - p) / (i + 1);
+        p += diff;
+        error = ( i - 1 ) * error / (i + 1) + pow(diff,2);
+
+    }
+
+    PutRNGstate();
+
+    error = 3 * sqrt(error);
+
+
+    *est = p;
+    *err = error;
+}
+
 /* Registration of the method
-static R_NativePrimitiveArgType mvtNormCpp_t[] = {
-    INTSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP
-};*/
+ static R_NativePrimitiveArgType mvtNormCpp_t[] = {
+ INTSXP, INTSXP, REALSXP, REALSXP, REALSXP, REALSXP, REALSXP
+ };*/
 
 static const R_CMethodDef cMethods[] = {
     {"mvtNormCpp", (DL_FUNC) &mvtNormCpp, 7},
+    {"mvTProbCpp", (DL_FUNC) &mvTProbCpp, 7},
     {NULL, NULL, 0}
 };
 
@@ -288,3 +519,4 @@ void R_init_mvPot(DllInfo *info)
     R_registerRoutines(info, cMethods, NULL, NULL, NULL);
     R_useDynamicSymbols(info, TRUE);
 }
+
